2c5d761499debea7c450858781b5e3c562e94720
Dorothea-B <barre.dorothea@gmail.com>
2022-03-01 21:04:54 +0100

    unit tests 1-5 done

--- src/3-find-index.js
+++ src/3-find-index.js
@@ -1,3 +1,10 @@
 export const findIndex = (array, value) => {
-  return
+  
+  return array.indexOf(value)
+
 }
+
+
+
+// it('should return the correct index of the first item', () => {
+//   expect(findIndex(items, 'Apple')).toEqual(0)
